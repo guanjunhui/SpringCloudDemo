55e76b6fb941a2d085b123c825887d223021f15a
guanjunhui <guanjunhui@300.cn>
2018-08-24 17:44:13 +0800

    springcloud整合freemarker

--- consumer/src/main/java/com/zqdl/controller/DemoController.java
+++ consumer/src/main/java/com/zqdl/controller/DemoController.java
@@ -1,26 +1,37 @@
 package com.zqdl.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zqdl.service.FeignClientService;
 
-@RestController
+@Controller
 public class DemoController {
 
 	@Autowired
 	private FeignClientService feignClientService;
 	
+	@ResponseBody
 	@RequestMapping(value="consumerDemo",method=RequestMethod.GET)
 	public String consumerDemo() {
 		return feignClientService.demoString();
 	}
 	
+	@ResponseBody
 	@RequestMapping("queryUserList")
 	public String queryUserList() {
 		return feignClientService.queryUserList();
+	}
+	
+	@RequestMapping("index")
+	public String index(Map<String, Object> map) {
+		map.put("user", feignClientService.queryUserList());
+		return "index";
 	}
 	
 }
---
+++ consumer/src/main/resources/templates/index.ftl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<#if user??>
+		<p>${user!}</p>
+	</#if>
+</body>
+</html>
